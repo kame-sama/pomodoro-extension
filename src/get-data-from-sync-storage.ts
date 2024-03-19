a422a0e70f209c948d900c5ed63fa337ca22e9c9
kame-sama <ad.bodeev@gmail.com>
2024-03-19 09:12:02 +0500

    Move types to env.d.ts

--- src/get-data-from-sync-storage.ts
+++ src/get-data-from-sync-storage.ts
@@ -1,54 +1,10 @@
-// Define interface for the first type of data
-interface UserData {
-  username: string;
-  email: string;
-}
-
-// Define interface for the second type of data
-interface SettingsData {
-  theme: string;
-  language: string;
-}
-
-// Define interface for the third type of data
-interface PreferencesData {
-  notifications: boolean;
-  darkMode: boolean;
-}
-
-// Create a map interface to ensure type safety when retrieving data
-interface StorageDataMap {
-  userData: UserData;
-  settingsData: SettingsData;
-  preferencesData: PreferencesData;
-}
-
-// Function to get data from Chrome storage
-export default function getDataFromSyncStorage<T extends keyof StorageDataMap>(
-  key?: T | T[]
-): Promise<
-  StorageDataMap[T] | StorageDataMap | Partial<StorageDataMap> | undefined
-> {
-  // No key
-  if (!key) {
-    return new Promise((resolve, reject) => {
-      chrome.storage.sync.get((data) => {
-        if (chrome.runtime.lastError) {
-          reject(chrome.runtime.lastError);
-        } else {
-          if (!Object.keys(data).length) {
-            resolve(undefined);
-          } else {
-            resolve(data as StorageDataMap);
-          }
-        }
-      });
-    });
-  }
-
-  // Single key
-  if (typeof key === 'string') {
-    return new Promise((resolve, reject) => {
+export default function getDataFromSyncStorage<
+  T extends keyof SyncStorageDataMap
+>(
+  key: T | T[]
+): Promise<SyncStorageDataMap[T] | Partial<SyncStorageDataMap> | undefined> {
+  return new Promise((resolve, reject) => {
+    if (typeof key === 'string') {
       chrome.storage.sync.get(key, (data) => {
         if (chrome.runtime.lastError) {
           reject(chrome.runtime.lastError);
@@ -56,25 +12,22 @@ export default function getDataFromSyncStorage<T extends keyof StorageDataMap>(
           if (!Object.keys(data).length) {
             resolve(undefined);
           } else {
-            resolve(data[key] as StorageDataMap[T]);
+            resolve(data[key] as SyncStorageDataMap[T]);
           }
         }
       });
-    });
-  }
-
-  // Multiple keys
-  return new Promise((resolve, reject) => {
-    chrome.storage.sync.get(key, (data) => {
-      if (chrome.runtime.lastError) {
-        reject(chrome.runtime.lastError);
-      } else {
-        if (!Object.keys(data).length) {
-          resolve(undefined);
+    } else {
+      chrome.storage.sync.get((data) => {
+        if (chrome.runtime.lastError) {
+          reject(chrome.runtime.lastError);
         } else {
-          resolve(data as Partial<StorageDataMap>);
+          if (!Object.keys(data).length) {
+            resolve(undefined);
+          } else {
+            resolve(data as Partial<SyncStorageDataMap>);
+          }
         }
-      }
-    });
+      });
+    }
   });
 }
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,23 @@
 /// <reference types="vite/client" />
+
+interface Settings {
+  pomodoro: number;
+  shortBreak: number;
+  longBreak: number;
+  autoStartBreaks: boolean;
+  autoStartPomodoros: boolean;
+  longBreakInterval: number;
+}
+
+interface Task {
+  title: string;
+  actPomodoros: number;
+  estPomodoros: number;
+  completed: boolean;
+}
+
+interface SyncStorageDataMap {
+  settings: Settings;
+  blocker: string[];
+  tasks: Task[];
+}
