d33283231386c12b5fdd4e514a6a948b4ad9b165
kame-sama <ad.bodeev@gmail.com>
2024-03-21 09:25:05 +0500

    Add notification on alarm

--- background.ts
+++ background.ts
@@ -1,6 +1,7 @@
 import createAlarmAndReturnDetails from './src/util/create-alarm-and-return-details';
 import getDataFromSessionStorage from './src/util/get-data-from-session-storage';
 import getDataFromSyncStorage from './src/util/get-data-from-sync-storage';
+import notificationIconUrl from '/icon-128.png';
 import storeDataInSessionStorage from './src/util/store-data-in-session-storage';
 import updateCurrentSession from './src/util/update-current-session';
 
@@ -20,6 +21,13 @@ chrome.alarms.onAlarm.addListener(async (event) => {
 
   updateCurrentSession(session as SessionStorageDataMap, settings);
   storeDataInSessionStorage(session);
+
+  chrome.notifications.create({
+    iconUrl: notificationIconUrl,
+    message: `Time to ${session.timer === 'pomodoro' ? 'focus' : 'rest'}`,
+    title: 'Pomodoro',
+    type: 'basic',
+  });
 
   if (
     (event.name === 'pomodoro' && settings.autoStartBreaks) ||
